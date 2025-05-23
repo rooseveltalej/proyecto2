fbdaf9cf6fe4de3e5e3d01c758cd96fc71f6322b
SrSenior <116299040+SrSenior@users.noreply.github.com>
2025-05-22 18:33:09 -0600

    Movimiento horizontal y salto
    Se añade movimiento horizontal al jugador, el cuál se está representando de momento como un rectangulo sin más.
    Se añade el salto del jugador

---
+++ Assets/Scripts/Player Controler.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+
+    public float velocidad = 5f;
+
+    public float fuerzaSalto = 10f;
+    public float longitudRaycast = 0.1f;
+    public LayerMask capaSuelo;
+
+    private bool enSuelo;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        //Este vendría a ser el movimiento horizontal del jugador
+        float velocidadX = Input.GetAxis("Horizontal")*Time.deltaTime*velocidad;
+        Vector3 posicion = transform.position;
+        transform.position = new Vector3(velocidadX + posicion.x, posicion.y, posicion.z);
+
+        //Todo esto se encarga del salto, tanto de detectar si está tocando el suelo, como de ejecutar el salto sino
+        RaycastHit2D  hit = Physics2D.Raycast(transform.position, Vector2.down, longitudRaycast, capaSuelo);
+        enSuelo = hit.collider != null;
+
+        if (enSuelo && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * longitudRaycast);
+    }
+}
