25edc40985e222845688fce8d4ccf00a624d643e
rooseveltalej <roperez@estudiantec.cr>
2025-05-27 08:09:26 -0600

    Refactor player controller for improved movement and animation handling

--- Assets/Scripts/Player Controler.cs
+++ Assets/Scripts/Player Controler.cs
@@ -19,20 +19,30 @@ public class PlayerControler : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    
+
     void Update()
     {
         //Este vendr�a a ser el movimiento horizontal del jugador
-        float velocidadX = Input.GetAxis("Horizontal")*Time.deltaTime*velocidad;
+        float velocidadX = Input.GetAxis("Horizontal") * Time.deltaTime * velocidad;
 
         //Este es para actualizar la animacion del jugador 
-        animator.SetFloat("movement", velocidadX*velocidad);
+        animator.SetFloat("movement", velocidadX * velocidad);
+
+
+        if (velocidadX > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (velocidadX < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
 
         Vector3 posicion = transform.position;
         transform.position = new Vector3(velocidadX + posicion.x, posicion.y, posicion.z);
 
         //Todo esto se encarga del salto, tanto de detectar si est� tocando el suelo, como de ejecutar el salto sino
-        RaycastHit2D  hit = Physics2D.Raycast(transform.position, Vector2.down, longitudRaycast, capaSuelo);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, longitudRaycast, capaSuelo);
         enSuelo = hit.collider != null;
 
         if (enSuelo && Input.GetKeyDown(KeyCode.Space))
