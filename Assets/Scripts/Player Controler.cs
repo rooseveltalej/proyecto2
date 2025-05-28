4f4ddf2583cae75bccd976732bb8369dc24fbef1
MingiHours <alecolochos@gmail.com>
2025-05-28 14:17:03 -0600

    Salto doble

--- Assets/Scripts/Player Controler.cs
+++ Assets/Scripts/Player Controler.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
@@ -5,18 +6,26 @@ public class PlayerControler : MonoBehaviour
 
     public float velocidad = 5f;
 
-    public float fuerzaSalto = 5f;
+    [SerializeField] private float fuerzaSalto = 5f;
     public float longitudRaycast = 0.1f;
-    public LayerMask capaSuelo;
-
+    
     private bool enSuelo;
-    private Rigidbody2D rb;
+    private bool salto;
 
-    public Animator animator;
+    [SerializeField] private Transform controladorSuelo;
+    [SerializeField] private Vector3 dimensionesCaja;
+    [SerializeField] private LayerMask capaSuelo;
+
+    [SerializeField] private int saltosExtraRestantes;
+    [SerializeField] private int saltosExtra;
+
+    private Rigidbody2D rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
 
@@ -41,22 +50,55 @@ public class PlayerControler : MonoBehaviour
         Vector3 posicion = transform.position;
         transform.position = new Vector3(velocidadX + posicion.x, posicion.y, posicion.z);
 
-        //Todo esto se encarga del salto, tanto de detectar si est� tocando el suelo, como de ejecutar el salto sino
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, longitudRaycast, capaSuelo);
-        enSuelo = hit.collider != null;
+        
+        // Code para detectar el salto
 
-        if (enSuelo && Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
+        enSuelo = Physics2D.OverlapBox(controladorSuelo.position, dimensionesCaja, 0f, capaSuelo);
+
+        if (enSuelo) {
+            saltosExtraRestantes = saltosExtra;
         }
 
-        animator.SetBool("enSuelo", enSuelo);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            salto = true;
+        }        
 
+    }
+
+    private void FixedUpdate()
+    {
+        Movimiento(salto);
+        salto = false;
+    }
+
+    private void Movimiento(bool salto)
+    {
+        if (salto) {
+            if (enSuelo)
+            {
+                Salto();
+            }
+            else
+            {
+                if (salto && saltosExtraRestantes > 0) {
+                    Salto();
+                    saltosExtraRestantes -= 1;
+                }
+            }
+        }
+    }
+
+    private void Salto()
+    {
+        rb.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
+        animator.SetBool("enSuelo", enSuelo);
+        salto = false;
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * longitudRaycast);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(controladorSuelo.position, dimensionesCaja);
     }
 }
