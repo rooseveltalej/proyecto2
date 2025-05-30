ad42eb49b44d46c95a6f0dd57c0c141ec65327e0
MingiHours <alecolochos@gmail.com>
2025-05-29 23:29:43 -0600

    Menu con historia

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,16 +1,48 @@
+using System;
+using TMPro;
+using UnityEditor.Rendering;
 using UnityEngine;
+using System.Collections;
+
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject panelHistoria;
+    public GameObject panelInicio;
+    public GameObject buttonContinue;
+    [SerializeField] private TMP_Text dialogueText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        panelHistoria.SetActive(false);
+        panelInicio.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void cargarHistoria()
+    {
+        buttonContinue.SetActive(false);
+        panelHistoria.SetActive(true);
+        panelInicio.SetActive(false);
+        StartCoroutine(ContarHistoria());
+    }
+
+    private IEnumerator ContarHistoria()
+    {
+        dialogueText.text = string.Empty;
+        string story = "Esta es la historia de un hamster que por circuntancias de la vida fue llevado por la lluvia y terminó siendo arrastrado hasta lo más profundo de las alcantarillas. Ahora es el momento de intentar regresar a casa...";
+
+        foreach (char ch in story)
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSecondsRealtime(0.05f);
+        }
+        buttonContinue.SetActive(true);
     }
 }
